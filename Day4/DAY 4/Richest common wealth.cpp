161b97f98b4224986acfedf87f90bb0816551022
vishnuvarthan1411 <vishnuvarthan26112007@gmail.com>
2026-07-10 21:02:54 +0530

    Day 4

---
+++ Day4/DAY 4/Richest common wealth.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int maximumWealth(vector<vector<int>>& accounts) {
+        int maxWealth=0;
+        for(int i=0;i<accounts.size();i++)
+        {
+            int sum=0;
+            for(int j=0;j<accounts[i].size();j++)
+            {
+                sum+=accounts[i][j];
+                maxWealth=max(sum,maxWealth);
+            }
+
+        }
+        return maxWealth;
+        
+    }
+};
