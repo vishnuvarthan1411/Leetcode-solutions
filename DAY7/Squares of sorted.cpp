bf7b16f04495b43f4aa7350995b6ccc996bdac79
vishnuvarthan1411 <vishnuvarthan26112007@gmail.com>
2026-07-13 10:22:37 +0530

    DAY 7

---
+++ DAY7/Squares of sorted.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<int> sortedSquares(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> ans(n);
+        int left=0,right=n-1;
+        int index=n-1;
+        while(left<=right)
+        {
+            int leftSq=nums[left]*nums[left];
+            int rightSq=nums[right]*nums[right];
+
+            if(leftSq>rightSq)
+            {
+                ans[index]=leftSq;
+                left++;
+            }
+            else
+            {
+                ans[index]=rightSq;
+                right--;
+            }
+            index--;
+        }
+        return ans;
+    }
+};
